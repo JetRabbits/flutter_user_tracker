cff0709a68fb1d5b7f94b3ca48e23828a6e93212
mormih <mormih@gmail.com>
2021-06-26 02:20:17 +0400

    register action

--- lib/flutter_user_tracker.dart
+++ lib/flutter_user_tracker.dart
@@ -1,5 +1,15 @@
 library flutter_user_tracker;
 
+import 'package:flutter_draft/flutter_draft.dart';
+import 'package:flutter_user_tracker/actions/track_action.dart';
+
+import 'integrations/google_tracker.dart';
+
 export 'actions/track_action.dart';
 export 'error_handling/catcher/lytics_report_mode.dart';
 export 'integrations/google_tracker.dart';
+
+setupUserTracker() async {
+  await GoogleTracker.initialize();
+  ActionRegister.addAction('tracker', TrackAction.toJson, TrackAction.fromJson);
+}
